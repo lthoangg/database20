23c0dcb26cf52833ac6b5dcee16acc23353423d2
lthoangg <hoanglt.bi9241@st.usth.edu.vn>
2020-07-07 16:58:14 +0700

    mod/update table

--- sql commands/CreateTables.sql
+++ sql commands/CreateTables.sql
@@ -5,25 +5,32 @@ DROP TABLE IF EXISTS teachers;
 drop table if exists students;
 drop table if exists classes;
 drop table if exists ASSESSMENT;
+drop table if exists employees;
+drop table if exists salary;
 
 create table Operators(
 	id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    fname varchar(45) not null,
-    lname VARCHAR(45) NOT NULL,
-    phonenumber varchar(10) not null,
-    address varchar(45) not null
+    employeeID int not null,
+    workShift varchar(12) not null
 );
 create table TEACHERS(
 	id int not null primary key auto_increment,
+    employeeID int not null, # reference to employees.id for me!
+    quality varchar(45) not null,
+    classID int not null
+);
+create table employees(
+    id int not null primary key auto_increment,
     fname varchar(45) not null,
     lname VARCHAR(45) NOT NULL,
-    age int not null,
-    phonenumber varchar(10) not null,
     address varchar(100) not null,
-    quality varchar(45) not null,
-    classID int not null,
-    wage int not null
-);
+    age int not null
+)
+create table salary(
+    id int not null primary key auto_increment,
+    employeeID int, --referencetoEmployees.ID please
+    salary int 
+)
 create table STUDENTS(
 	id int not null primary key auto_increment,
     fname varchar(45) not null,
@@ -38,8 +45,8 @@ create table CLASSES(
     classname varchar(2) not null,
     subject varchar(15) not null,
     schedule varchar(12) not null,
-    timestart time not null,
-    timeend time not null,
+    timestart varchar(6) not null,
+    timeend varchar(6) not null,
     teacherID int not null,
 	fee int not null
 );
